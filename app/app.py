215aec2aaba1eca1d256aff8db99722eab942714
Paul <paul.bagnis@gmail.com>
2022-04-21 15:13:31 +0200

    [add] Add API routes for data query

--- app/app.py
+++ app/app.py
@@ -1,10 +1,40 @@
-From flask import Flask
+from flask import Flask, request
+from tools.mongo_connector import Mongo
+import json
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world:
-    return 'Hey, we have Flask in a Docker container!'
+mongo = Mongo()
+btc_collection = mongo.getCollection("btc")
 
+btc_columns = { '_id', 'High', 'Low', 'Open', 'Close', 'Volume' }
+pred_columns = { '_id', 'Predicted High', 'Predicted Low', 'Predicted Open', 'Predicted Close', 'Predicted Volume' }
 
-if __name == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+@app.route('/docs', methods=['GET'])
+def docs():
+    response = btc_collection.find({}).limit(request.args[0]) if request.args[0] else btc_collection.find({})
+    return json.dumps(response), 200
+
+@app.route('/btcPrice', methods=['GET'])
+def btcPrice():
+    response = btc_collection.find(btc_columns).limit(request.args[0]) if request.args[0] else btc_collection.find(btc_columns)
+    return json.dumps(response), 200
+
+@app.route('/predPrice', methods=['GET'])
+def predPrice():
+    response = btc_collection.find(pred_columns).limit(request.args[0]) if request.args[0] else btc_collection.find(pred_columns)
+    return json.dumps(response), 200
+
+@app.route('/lastRow', methods=['GET'])
+def predPrice():
+    response = btc_collection.find().sort({'_id':-1}).limit(1)
+    return json.dumps(response), 200
+
+'''
+route 1 : Retourner toutes les valeurs de la DB
+route 2 : retourner vrai prix btc + Date
+route 3 : retourner prediction + date
+route 4 : retourner que dernière prédiction
+
+TODO : Ajouter la tendance dans le Db
+'''
