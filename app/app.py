52948e0f487a02e9467ac8d97f947dc6a61a6b07
JeremYnov <jeremy.richard@ynov.com>
2022-04-27 14:51:23 +0200

    [add] interface vue3

--- app/app.py
+++ app/app.py
@@ -2,8 +2,11 @@ from flask import Flask, request
 from tools.mongo_connector import Mongo
 import json
 from bson import json_util
+import os
 
 app = Flask(__name__)
+
+
 
 mongo = Mongo()
 btc_collection = mongo.getCollection("btc")
@@ -31,6 +34,10 @@ def lastRow():
     response = btc_collection.find().sort({'_id':-1}).limit(1)
     return json.dumps( list(response), default=json_util.default), 200
 
+@app.route('/')
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
+
 '''
 route 1 : Retourner toutes les valeurs de la DB
 route 2 : retourner vrai prix btc + Date
@@ -41,4 +48,6 @@ TODO : Ajouter la tendance dans le Db
 '''
 
 if __name__ == "__main__":
-	app.run()
+	# app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
